5850615a5656c81570c2bb5dbf6d1b2e1c8f6a5d
ChrisBigg <chri.bignardi@gmail.com>
2024-05-06 22:33:38 +0200

    Update Scene Builder

---
+++ src/main/java/com/bignardi/barbermanager/controller/OverviewController.java
@@ -0,0 +1,11 @@
+package com.bignardi.barbermanager.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class OverviewController {
+    @FXML
+    private TableView<String> firstDayTable;
+
+
+}
